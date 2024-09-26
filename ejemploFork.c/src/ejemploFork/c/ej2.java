9ebdfe23362c44a2446b660797d07fb0cd24e5e0
raupersan <raulps1798@gmail.com>
2024-09-26 14:27:56 +0200

    Ejercicio 2 casi terminado

--- ejemploFork.c/src/ejemploFork/c/ej2.java
+++ ejemploFork.c/src/ejemploFork/c/ej2.java
@@ -12,8 +12,21 @@ int main() {
 	pid2=fork();
 	if(pid2==0) {
 		pidh1=fork();
-		if(pidh1==0) {}
+		if(pidh1==0) {
+			printf("PID del proceso: %d\nPPID del proceso: %d\n", getpid(), getppid());
+			exit(2);
+		}
+		pidh2=fork();
+		if(pidh2==0) {
+			printf("PID del proceso: %d\nPPID del proceso: %d\n", getpid(), getppid());
+			exit(2);
+		}
+		printf("PID del proceso: %d\nPPID del proceso: %d\nPID de los procesos hijos que ha creado: %d y %d\n", getpid(), getppid());
+		wait(NULL);
+		wait(NULL);
 	}
+	wait(NULL);
+	wait(NULL);
 	
 	return 0;
 }

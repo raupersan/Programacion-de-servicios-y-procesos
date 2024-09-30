f765e8cc525fb269d00844903b5a56b5078e705c
raupersan <raulps1798@gmail.com>
2024-09-30 11:09:40 +0200

    ejercicios casi terminados

--- ejemploFork.c/src/ejemploFork/c/codigoCorrecto.c
+++ ejemploFork.c/src/ejemploFork/c/codigoCorrecto.c
@@ -4,13 +4,13 @@
 
 int main() {
 	pid_t pid;
-// el pid es de tipo pid_t
+// El pid es de tipo pid_t
 	pid = fork();
 	if (pid == 0) {
 // El proceso hijo empieza con pid=0
-	perror("fork failed");
 	exit(1);
 	}
+//Proceso Padre
 	printf("Hola, ¿qué tal? Todo bien, gracias.\\n");
 	if (pid == 0) {
 	printf("\\tSoy el proceso hijo, mi PID es %d\\n", getpid());
